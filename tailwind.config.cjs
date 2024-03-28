097c854acd1932265a321f2ca1e24dc89bcd4e56
kamahorris11 <165331582+kamahorris11@users.noreply.github.com>
2024-03-29 00:13:00 +0300

    wesbite

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{jsx,js}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        'coming-soon': ['"Coming Soon"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+};
